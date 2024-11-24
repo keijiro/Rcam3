4c01ae4689ab0f56bc293e707ac380dffbc54c5c
Keijiro Takahashi <keijiro@gmail.com>
2024-11-24 15:25:35 +0900

    Cosmetic changes

--- Rcam3Controller/Assets/Shaders/Encoder.hlsl
+++ Rcam3Controller/Assets/Shaders/Encoder.hlsl
@@ -1,14 +1,15 @@
 #include "Packages/jp.keijiro.rcam3.common/Shaders/RcamCommon.hlsl"
 
-void EncodeFrame_float(float2 UV,
-                   UnityTexture2D YTex,
-                   UnityTexture2D CbCrTex,
-                   UnityTexture2D MaskTex,
-                   UnityTexture2D DepthTex,
-                   UnityTexture2D ScoreTex,
-                   float AspectFix,
-                   float2 DepthRange,
-                   out float4 Output)
+void EncodeFrame_float
+  (float2 UV,
+   UnityTexture2D YTex,
+   UnityTexture2D CbCrTex,
+   UnityTexture2D MaskTex,
+   UnityTexture2D DepthTex,
+   UnityTexture2D ScoreTex,
+   float AspectFix,
+   float2 DepthRange,
+   out float4 Output)
 {
     float4 tc = frac(UV.xyxy * float4(1, 1, 2, 2));
 
@@ -20,7 +21,7 @@ void EncodeFrame_float(float2 UV,
     float2 cbcr = tex2D(CbCrTex, tc.zy).xy;
     float depth = tex2D(DepthTex, tc.zw).x;
     float mask = tex2D(MaskTex, tc.zw).x;
-    float conf = tex2D(ScoreTex, tc.zw).x;
+    float score = tex2D(ScoreTex, tc.zw).x;
 
     // Color plane
     float3 c1 = RcamYCbCrToSRGB(y, cbcr);
@@ -29,7 +30,7 @@ void EncodeFrame_float(float2 UV,
     float3 c2 = RcamEncodeDepth(depth, DepthRange);
 
     // Mask plane
-    float3 c3 = float3(mask, conf * 128, 0);
+    float3 c3 = float3(mask, score * 128, 0);
 
     // Output
     float3 srgb = tc.x < 0.5 ? c1 : (tc.y < 0.5 ? c2 : c3);
