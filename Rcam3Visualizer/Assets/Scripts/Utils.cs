8cef39e085fa8da5206827c693a98245f255b814
Keijiro Takahashi <keijiro@gmail.com>
2024-12-03 23:06:39 +0900

    SkyGradient

--- Rcam3Visualizer/Assets/Scripts/Utils.cs
+++ Rcam3Visualizer/Assets/Scripts/Utils.cs
@@ -5,6 +5,7 @@ namespace Rcam3 {
 static class ShaderID
 {
     public static readonly int DepthRange = Shader.PropertyToID("_DepthRange");
+    public static readonly int Gradient = Shader.PropertyToID("_Gradient");
     public static readonly int LutTexture = Shader.PropertyToID("_LutTexture");
 }
 
