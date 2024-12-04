152cc16e29270cd4f5f0b69622806519e74bf734
Keijiro Takahashi <keijiro@gmail.com>
2024-12-04 22:16:08 +0900

    Cosine gradient

--- Rcam3Visualizer/Assets/Shaders/GradientSky.hlsl
+++ Rcam3Visualizer/Assets/Shaders/GradientSky.hlsl
@@ -4,8 +4,8 @@
 void GradientSky_float
   (float3 UVW, float Dither, float4x4 Gradient, out float3 Output)
 {
-    float n1 = SimplexNoise(UVW * 1 + float3(0, _Time.y * -0.3, 0)) * 0.10;
-    float n2 = SimplexNoise(UVW * 2 + float3(0, _Time.y * -0.3, 0)) * 0.04;
-    float x = saturate(UVW.y + n1 + n2 + 0.5 + Dither);
-    Output = CosineGradient(Gradient, x);
+    float n1 = SimplexNoise(UVW * 0.5 + float3(0, _Time.y * -0.2, 0)) * 0.20;
+    float n2 = SimplexNoise(UVW * 1.0 + float3(0, _Time.y *  0.1, 0)) * 0.14;
+    float x = saturate(UVW.y + n1 + n2 + 0.5 + Dither / 64);
+    Output = SRGBToLinear(CosineGradient(Gradient, x));
 }
